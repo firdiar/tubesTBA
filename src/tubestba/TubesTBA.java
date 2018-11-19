bff107c1acfa8b2329f19261f492a20b3c7cc260
liuslangobelen <langobelenlius@gmail.com>
2018-11-19 13:59:22 +0700

    fixing is object

--- src/tubestba/TubesTBA.java
+++ src/tubestba/TubesTBA.java
@@ -122,12 +122,12 @@ public class TubesTBA {
         if(isObject(kata)){
             return 'o';
         }
-  
+        
 
         if(isPredikat(kata)){
             return 'p';
         }
-        return Character.MIN_VALUE;
+        return 'w';
     }
 
 
@@ -283,21 +283,29 @@ public class TubesTBA {
 
         StateFA q0 = new StateFA("is");
         StateFA q1 = new StateFA("b");
-        StateFA q2 = new StateFA("oau");
-        StateFA q3 = new StateFA("ltjsk");
-        StateFA q4 = new StateFA("auo",true);
+        StateFA q2 = new StateFA("a");
+        StateFA q3 = new StateFA("o");
+        StateFA q4 = new StateFA("u");
+        StateFA q5 = new StateFA("s");
+        StateFA q7 = new StateFA("jt");
+        StateFA q6 = new StateFA("l");
+        StateFA q8 = new StateFA("k");
+        StateFA q9 = new StateFA("auo",true);
+
         q0.addTransition('b', q1);
         q1.addTransition('a', q2);
-        q1.addTransition('o', q2);
-        q1.addTransition('u', q2);
-        q2.addTransition('l', q3);
-        q2.addTransition('t', q3);
-        q2.addTransition('j', q3);
-        q2.addTransition('s', q3);
-        q2.addTransition('k', q3);
-        q3.addTransition('a', q4);
-        q3.addTransition('u', q4);
-        q3.addTransition('o', q4);
+        q1.addTransition('o', q3);
+        q1.addTransition('u', q4);
+        q2.addTransition('s', q5);
+        q2.addTransition('t', q7);
+        q2.addTransition('j', q7);
+        q3.addTransition('l', q6);
+        q4.addTransition('k', q8);
+        q6.addTransition('a', q9);
+        q8.addTransition('u', q9);
+        q5.addTransition('o', q9);
+        q7.addTransition('u', q9);
+        
         
         StateFA current = q0;
         int i =0;
@@ -311,20 +319,21 @@ public class TubesTBA {
     }
 
     public static boolean isSubject(String kata){
-            // aku ani abi ali ari
+            // aku ani abi ali ari 
 
             StateFA q0 = new StateFA("is");
             StateFA q1 = new StateFA("a");
             StateFA q2 = new StateFA("knblr");
+            StateFA q4 = new StateFA("q4");
             StateFA q3 = new StateFA("iu",true);
             q0.addTransition('a', q1);
-            q1.addTransition('k', q2);
+            q1.addTransition('k', q4);
             q1.addTransition('n', q2);
             q1.addTransition('b', q2);
             q1.addTransition('l', q2);
             q1.addTransition('r', q2);
             q2.addTransition('i', q3);
-            q2.addTransition('u', q3);
+            q4.addTransition('u', q3);
 
             StateFA current = q0;
             int i=0;
